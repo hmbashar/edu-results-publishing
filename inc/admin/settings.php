ea7b863e1cac1f4f0c542cd218338a6db34489e0
hmbashar <hmbashar@gmail.com>
2023-09-06 01:53:09 +0600

    restore logo field

--- inc/admin/settings.php
+++ inc/admin/settings.php
@@ -1,21 +1,24 @@
-<?php 
+<?php
 
 namespace inc\admin\settings;
 
-class EDUResultSettings {
+class EDUResultSettings
+{
 
     private $prefix;
     private $textdomain;
-    
-    public function __construct($eduResultPublishing) {
+
+    public function __construct($eduResultPublishing)
+    {
         $this->textdomain = $eduResultPublishing->getTextDomain();
         $this->prefix = $eduResultPublishing->getPrefix();
 
         add_action('admin_menu', array($this, 'addSubmenuPage'));
         add_action('admin_init', array($this, 'registerSettings'));
     }
-    
-    public function addSubmenuPage() {
+
+    public function addSubmenuPage()
+    {
         $parentSlug = 'edit.php?post_type=' . $this->prefix . 'results';
         $pageTitle = __('Edu Result Settings', $this->textdomain);
         $menuTitle = __('Settings', $this->textdomain);
@@ -25,7 +28,8 @@ class EDUResultSettings {
 
         add_submenu_page($parentSlug, $pageTitle, $menuTitle, $capability, $menuSlug, $function);
     }
-    public function renderSettingsPage() {
+    public function renderSettingsPage()
+    {
         ?>
         <div class="wrap">
             <h2>Edu Results Settings</h2>
@@ -33,35 +37,52 @@ class EDUResultSettings {
                 <?php settings_fields('edu_results_settings_group'); ?>
                 <?php do_settings_sections('edu_results_settings'); ?>
                 <?php
-                    // Add nonce field
-                    wp_nonce_field('edu_results_settings_nonce', 'edu_results_settings_nonce_field');
+                // Add nonce field
+                wp_nonce_field('edu_results_settings_nonce', 'edu_results_settings_nonce_field');
                 ?>
                 <?php submit_button(); ?>
             </form>
         </div>
         <?php
     }
-    
-    public function registerSettings() {
-               
+
+    public function registerSettings()
+    {
+        // Add a new option for the logo
+        register_setting('edu_results_settings_group', 'edu_results_logo', array(
+            'type' => 'string',
+            // Store the attachment URL as a string
+            'sanitize_callback' => 'esc_url_raw',
+            // Sanitize the URL
+            'show_in_rest' => true, // Show the field in the REST API
+        )
+        );
         // Add a new option for Collage Name
         register_setting('edu_results_settings_group', 'edu_results_collage_name', 'sanitize_text_field');
-        
+
         // Add a new option for Collage Registration Number
         register_setting('edu_results_settings_group', 'edu_results_collage_registration_number', 'sanitize_text_field');
-        
+
         // Add a new option for Collage Since Year
         register_setting('edu_results_settings_group', 'edu_results_collage_since_year', 'sanitize_text_field');
-        
+
         // Add a new option for Collage Address
         register_setting('edu_results_settings_group', 'edu_results_collage_address', 'sanitize_text_field');
-        
+
         // Add a new option for Collage Phone Number
         register_setting('edu_results_settings_group', 'edu_results_collage_phone_number', 'sanitize_text_field');
-        
+
         // Add a new option for Collage Email Address
         register_setting('edu_results_settings_group', 'edu_results_collage_email_address', 'sanitize_email');
-        
+
+
+        add_settings_field(
+            'edu_results_logo',
+            'Logo',
+            array($this, 'logoFieldCallback'),
+            'edu_results_settings',
+            'edu_results_settings_section'
+        );
         
         add_settings_section(
             'edu_results_settings_section',
@@ -69,8 +90,8 @@ class EDUResultSettings {
             array($this, 'settingsSectionCallback'),
             'edu_results_settings'
         );
-    
-        
+
+
         // Add new settings fields for the new options
         add_settings_field(
             'edu_results_collage_name',
@@ -79,7 +100,7 @@ class EDUResultSettings {
             'edu_results_settings',
             'edu_results_settings_section'
         );
-        
+
         add_settings_field(
             'edu_results_collage_registration_number',
             'Collage Registration Number',
@@ -87,7 +108,7 @@ class EDUResultSettings {
             'edu_results_settings',
             'edu_results_settings_section'
         );
-        
+
         add_settings_field(
             'edu_results_collage_since_year',
             'Collage Since Year',
@@ -95,7 +116,7 @@ class EDUResultSettings {
             'edu_results_settings',
             'edu_results_settings_section'
         );
-        
+
         add_settings_field(
             'edu_results_collage_address',
             'Collage Address',
@@ -103,7 +124,7 @@ class EDUResultSettings {
             'edu_results_settings',
             'edu_results_settings_section'
         );
-        
+
         add_settings_field(
             'edu_results_collage_phone_number',
             'Collage Phone Number',
@@ -111,7 +132,7 @@ class EDUResultSettings {
             'edu_results_settings',
             'edu_results_settings_section'
         );
-        
+
         add_settings_field(
             'edu_results_collage_email_address',
             'Collage Email Address',
@@ -119,16 +140,18 @@ class EDUResultSettings {
             'edu_results_settings',
             'edu_results_settings_section'
         );
-        
-   
 
-        
+
+
+
+
         // Add nonce validation
         add_action('admin_init', array($this, 'validateNonce'));
 
     }
-    
-    public function validateNonce() {
+
+    public function validateNonce()
+    {
         if (isset($_POST['edu_results_settings_nonce_field'])) {
             $nonce = sanitize_text_field($_POST['edu_results_settings_nonce_field']);
             if (!wp_verify_nonce($nonce, 'edu_results_settings_nonce')) {
@@ -137,7 +160,8 @@ class EDUResultSettings {
         }
     }
 
-    public function logoFieldCallback() {
+    public function logoFieldCallback()
+    {
         $logoURL = get_option('edu_results_logo');
         ?>
         <input type="text" name="edu_results_logo" value="<?php echo esc_url($logoURL); ?>" id="edu_results_logo">
@@ -145,72 +169,79 @@ class EDUResultSettings {
         <br>
         <img src="<?php echo esc_url($logoURL); ?>" alt="Logo Preview" id="logo_preview" style="max-width: 200px;">
         <script>
-        jQuery(document).ready(function($) {
-            $('#upload_logo_button').click(function(e) {
-                e.preventDefault();
-    
-                var customUploader = wp.media({
-                    title: 'Upload Logo',
-                    button: {
-                        text: 'Use this Logo'
-                    },
-                    library: {
-                        type: 'image'
-                    },
-                    multiple: false
+            jQuery(document).ready(function ($) {
+                $('#upload_logo_button').click(function (e) {
+                    e.preventDefault();
+
+                    var customUploader = wp.media({
+                        title: 'Upload Logo',
+                        button: {
+                            text: 'Use this Logo'
+                        },
+                        library: {
+                            type: 'image'
+                        },
+                        multiple: false
+                    });
+
+                    customUploader.on('select', function () {
+                        var attachment = customUploader.state().get('selection').first().toJSON();
+                        $('#edu_results_logo').val(attachment.url);
+                        $('#logo_preview').attr('src', attachment.url);
+                    });
+
+                    customUploader.open();
                 });
-    
-                customUploader.on('select', function() {
-                    var attachment = customUploader.state().get('selection').first().toJSON();
-                    $('#edu_results_logo').val(attachment.url);
-                    $('#logo_preview').attr('src', attachment.url);
-                });
-    
-                customUploader.open();
             });
-        });
         </script>
         <p class="description">Upload your logo here.</p>
         <?php
     }
-    
 
-    public function settingsSectionCallback() {
+
+    public function settingsSectionCallback()
+    {
         echo 'These are the general settings for the Edu Results plugin.';
     }
 
- 
 
-    public function collageNameFieldCallback() {
+
+    public function collageNameFieldCallback()
+    {
         $collageName = get_option('edu_results_collage_name');
         echo '<input type="text" name="edu_results_collage_name" value="' . esc_attr($collageName) . '" />';
     }
-    
-    public function collageRegistrationNumberFieldCallback() {
+
+    public function collageRegistrationNumberFieldCallback()
+    {
         $collageRegNumber = get_option('edu_results_collage_registration_number');
         echo '<input type="text" name="edu_results_collage_registration_number" value="' . esc_attr($collageRegNumber) . '" />';
     }
-    
-    public function collageSinceYearFieldCallback() {
+
+    public function collageSinceYearFieldCallback()
+    {
         $collageSinceYear = get_option('edu_results_collage_since_year');
         echo '<input type="text" name="edu_results_collage_since_year" value="' . esc_attr($collageSinceYear) . '" />';
     }
-    
-    public function collageAddressFieldCallback() {
+
+    public function collageAddressFieldCallback()
+    {
         $collageAddress = get_option('edu_results_collage_address');
         echo '<input type="text" name="edu_results_collage_address" value="' . esc_attr($collageAddress) . '" />';
     }
-    
-    public function collagePhoneNumberFieldCallback() {
+
+    public function collagePhoneNumberFieldCallback()
+    {
         $collagePhoneNumber = get_option('edu_results_collage_phone_number');
         echo '<input type="text" name="edu_results_collage_phone_number" value="' . esc_attr($collagePhoneNumber) . '" />';
     }
-    
-    public function collageEmailAddressFieldCallback() {
+
+    public function collageEmailAddressFieldCallback()
+    {
         $collageEmailAddress = get_option('edu_results_collage_email_address');
         echo '<input type="email" name="edu_results_collage_email_address" value="' . esc_attr($collageEmailAddress) . '" />';
     }
-    
-    
-    
+
+
+
 }
