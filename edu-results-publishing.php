407279919616fb0e3899488fc7b7d6e77cd22574
hmbashar <hmbashar@gmail.com>
2024-01-07 21:33:02 +0600

    removed mark function

--- edu-results-publishing.php
+++ edu-results-publishing.php
@@ -490,26 +490,6 @@ class CBEDUResultPublishing
     
 }
 
-function convert_marks_to_grade($marks)
-{
-    if ($marks >= 80) {
-        return array('A+', 5.00);
-    } elseif ($marks >= 70) {
-        return array('A', 4.00);
-    } elseif ($marks >= 60) {
-        return array('A-', 3.50);
-    } elseif ($marks >= 50) {
-        return array('B', 3.00);
-    } elseif ($marks >= 40) {
-        return array('C', 2.00);
-    } elseif ($marks >= 33) {
-        return array('D', 1.00);
-    } else {
-        return array('F', 0.00);
-    }
-}
-
-
 
 require_once CBEDU_RESULT_DIR . 'inc/custom-fields.php';
 
